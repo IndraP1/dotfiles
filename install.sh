eee361ad32576933d97fdc22cef2516c4f123550
indra prasetyo <indra.prasetyo@calgaryscientific.com>
2015-12-16 15:12:51 -0700

    Created install script and README

---
+++ install.sh
@@ -0,0 +1,7 @@
+ln -s dotfiles/bashrc $HOME/.bashrc 
+ln -s dotfiles/screenrc $HOME/.screenrc 
+ln -s dotfiles/nvimrc $HOME/.nvimrc
+ln -s dotfiles/gitconfig $HOME/.gitconfig 
+ln -s dotfiles/gitignore $HOME/.gitignore 
+
+echo "Symbolic links successfully created. Remember to source your bashrc."
